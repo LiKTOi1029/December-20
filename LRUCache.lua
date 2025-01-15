aa72136f00deeadc3ed9a58f4d674e50e9aed9f2
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-15 10:09:34 +0330

    additional bugfixes and troubleshooting

--- LRUCache.lua
+++ LRUCache.lua
@@ -6,18 +6,19 @@ function fSHOW(tab)
 	io.write("}".."\n")
 end
 function fINPUT(choice,tab)
-	for _, string1 in ipairs(tab) do
-		if choice == string1 then io.write("Fetched " .."<"..string1..">"); table.insert(tab, 1, choice); break 
-		else table.insert(tab, 1, choice); table.remove(tab, 4); io.write("Popped \""..tostring(tab[4]).."\" from the stack") break end
+	for num1, string1 in ipairs(tab) do
+		if choice == string1 then io.write(">>Fetched " .."<"..string1..">\n"); table.insert(tab, 1, choice); table.remove(tab,4) break
+		elseif #tab == 3 and choice ~= tab[3] then table.insert(tab, 1, choice); if tab[4] then io.write(">>Popped \""..tostring(tab[4]).."\" from the cache (cache can only hold "..tostring(#tab-1)..")\n") end table.remove(tab, 4); break 
+		elseif choice ~= tab[#tab] then table.insert(tab, 1, choice); io.write(">>No matches found, saving to cache\n") break end
 	end
-	if #tab == 0 then table.insert(tab, 1, choice) end
+	if #tab == 0 then table.insert(tab, 1, choice); io.write(">>Empty cache, saving to cache\n") end
 end
 function __MAIN__()
 	local cache = {}
 	repeat
-		io.write("Type SHOW to show to the table. Type INPUT to save or fetch from a table. Type EXIT to EXIT\n")
+		io.write(">>Type SHOW to show to the table. Type INPUT to save or fetch from a table. Type EXIT to EXIT\n")
 		local option = io.read("*l"):gsub("\n", "")
-		if option == "INPUT" then io.write("Input something: \n"); local input = io.read("*l"):gsub("\n", ""); fINPUT(input,cache)
+		if option == "INPUT" then io.write(">>Input something: \n"); local input = io.read("*l"):gsub("\n", ""); fINPUT(input,cache)
 		elseif option == "SHOW" then fSHOW(cache)
 		elseif option == "EXIT" then break end
 	until true == false
